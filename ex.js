612266d3f3c635503aaa8b797d03469a16e60cd1
flwps <62161519+flwps@users.noreply.github.com>
2020-10-09 16:57:16 +0900

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,10 @@
+function setMenu(_menu) {
+    var filterButtons = document.querySelectorAll("nav li");
+    filterButtons.forEach(function (filterButton) {
+        filterButton.classList.remove("on");
+        }
+    );
+    document.querySelector("nav li." + _menu).classList.add("on");
+    document.querySelector("main").className = _menu;
+}
+  
